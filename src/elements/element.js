3738da7f6709f900ae1a44a312117272714103ad
sojin0106 <97339665+sojin0106@users.noreply.github.com>
2022-02-23 22:48:27 +0900

    Delete src directory

--- src/elements/element.js
+++
@@ -1,7 +0,0 @@
-import Button from "./Button";
-import Input from "./Input";
-import Image from "./Image";
-import Text from "./Text";
-import LoginBox from "./LoginBox";
-
-export { Button, Input, Image, Text, LoginBox };
--- src/pages/page.js
+++
@@ -1,9 +0,0 @@
-import Main from "./Main";
-import Login from "./Login";
-import Signup from "./Signup";
-import CartList from "./CartList";
-import NotFound from "./NotFound";
-import Detail from "./Detail"
-
-
-export { Main, Login, Signup, CartList, NotFound, Detail };
